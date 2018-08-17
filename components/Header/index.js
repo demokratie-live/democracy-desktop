2e961ef5cc744176286f500b4ca5b3753044d1e5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-08-17 15:59:20 +0200

    header ant rows/cols

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,34 +1,40 @@
 import { withRouter } from 'next/router';
+import { Row, Col } from 'antd';
 
-import Link from "../shared/Link";
+import Link from '../shared/Link';
 
 const Header = ({ router: { pathname } }) => (
   <header>
-      <Link  as={`/gesetz/234877/Some-Title`} href={"/details?title=testTitle"}>
-        TestLink
-      </Link>
-    {/*<Row>
-      <Col3>
-        <Bubble>DEMOCRACY</Bubble>
-      </Col3>
-      <Col6>
-        <SearchField>Suche</SearchField>
-      </Col6>
-      <Col3>
-        <Filter />
+    <Link as={`/gesetz/234877/Some-Title`} href={`/details?title=testTitle`}>
+      TestLink
+    </Link>
+    <Row>
+      <Col span={3}>
+        ABC
+        {/*<Bubble>DEMOCRACY</Bubble>*/}
+      </Col>
+      <Col span={6}>
+        EFG
+        {/*<SearchField>Suche</SearchField>*/}
+      </Col>
+      <Col span={3}>
+        HIJ
+        {/*<Filter />
         <Download>App Downloaden</Download>
-        <Support>Unterstützen</Support>
-      </Col3>
+        <Support>Unterstützen</Support>*/}
+      </Col>
     </Row>
     <Row>
-      <Col3>
-        <PeriodSelect>19. Legislaturperiode ('17-'21)</PeriodSelect>
-      </Col3>
-      <Col6>
-        <ListSelect />
-      </Col6>
-      <Col3 />
-    </Row>*/}
+      <Col span={3}>
+        KLM
+        {/*<PeriodSelect>19. Legislaturperiode ('17-'21)</PeriodSelect>*/}
+      </Col>
+      <Col span={6}>
+        NOP
+        {/*<ListSelect />*/}
+      </Col>
+      <Col span={3} />
+    </Row>
   </header>
 );
 
