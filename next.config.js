f86656f9db4642746eb62f0f46022bb67f47e296
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-08-17 16:17:17 +0200

    ant design css not loading...

--- next.config.js
+++ next.config.js
@@ -39,3 +39,10 @@ module.exports = withSourceMaps(
     },
   }),
 );
+
+const withCss = require('@zeit/next-css')
+
+/* // fix: prevents error when .css files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = (file) => {}
+}*/
