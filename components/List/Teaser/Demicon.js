1237eeb870262287bd710d7aa87384bf2639e272
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-08-22 16:18:02 +0200

    tooltips for demicons

--- components/List/Teaser/Demicon.js
+++ components/List/Teaser/Demicon.js
@@ -1,3 +1,4 @@
+import { Tooltip } from 'antd';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -11,21 +12,30 @@ const getChar = type => {
 const Circle = styled.i`
   background-color: ${({ theme }) => theme.colors.disabled};
   border-radius: 50%;
-  text-align: center;
   padding: 15px;
-  width: 50px;
   &:before {
     font-family: 'democracy';
-    margin: auto;
-    font-size: ${({ theme }) => theme.fontSizes.large}px;
+    font-size: ${({ theme }) => theme.fontSizes.default};
+    color: ${({ theme }) => theme.backgrounds.primary};
     content: '${({ type }) => getChar(type)}';
+  }
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.link};
   }
 `;
 
-const Demicon = () => <Circle />;
+const Demicon = ({ tooltip }) => (
+  <Tooltip placement="bottom" title={tooltip}>
+    <Circle />
+  </Tooltip>
+);
 
 Demicon.propTypes = {
   type: PropTypes.string.isRequired,
+  tooltip: PropTypes.string,
+};
+Demicon.defaultProps = {
+  tooltip: 'The Tooltip of the Demicon',
 };
 
 export default Demicon;
