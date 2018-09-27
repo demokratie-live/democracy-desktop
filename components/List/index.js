42ce245957ace2fe80562c58cead3fa337e948e3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-09-27 12:35:57 +0200

    corrected meta tags - but there are rendered client side -> nogo -> why?

--- components/List/index.js
+++ components/List/index.js
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-import getConfig from 'next/config';
-import Head from 'next/head';
 
 // Components
 import ListTypesList from './ListTypesList';
@@ -9,56 +7,21 @@ import SearchList from './SearchList';
 // Context
 import { Consumer as SearchConsumer } from 'Context/search';
 
-const {
-  publicRuntimeConfig: { PAGE_TITLE, DOMAIN_DESKTOP },
-} = getConfig();
-
 class List extends Component {
   render() {
     return (
-      <>
-        {/* <Head>
-          <meta key="og-title" property="og:title" content={PAGE_TITLE} />
-          <meta key="page-topic" name="page-topic" content={PAGE_TITLE} />
-          <meta key="og-url" property="og:url" content={DOMAIN_DESKTOP} />
-          <meta
-            key="og-image"
-            property="og:image"
-            content={`${DOMAIN_DESKTOP}/static/images/democracy.png`}
-          />
+      <SearchConsumer>
+        {consumerProps => {
+          if (!consumerProps) return null;
+          const { term } = consumerProps;
 
-          <meta
-            key="description"
-            name="description"
-            content="Die offizielle Browserversion der DEMOCRACY App – das 10X-Improvement für unsere Demokratie. DEMOCRACY räumt jedem nutzenden Bundesbürger die Möglichkeit ein, sich über die aktuellen Parlamentsabstimmungen des deutschen Bundestags zu informieren und selbst abzustimmen"
-          />
-          <meta
-            key="dc-description"
-            name="DC.Description"
-            content="Die offizielle Browserversion der DEMOCRACY App – das 10X-Improvement für unsere Demokratie. DEMOCRACY räumt jedem nutzenden Bundesbürger die Möglichkeit ein, sich über die aktuellen Parlamentsabstimmungen des deutschen Bundestags zu informieren und selbst abzustimmen"
-          />
-          <meta
-            key="og-description"
-            property="og:description"
-            content="Die offizielle Browserversion der DEMOCRACY App – das 10X-Improvement für unsere Demokratie. DEMOCRACY räumt jedem nutzenden Bundesbürger die Möglichkeit ein, sich über die aktuellen Parlamentsabstimmungen des deutschen Bundestags zu informieren und selbst abzustimmen"
-          />
-
-          <meta key="page-type" name="page-type" content="website" />
-          <meta key="og-type" property="og:type" content="website" />
-        </Head> */}
-        <SearchConsumer>
-          {consumerProps => {
-            if (!consumerProps) return null;
-            const { term } = consumerProps;
-
-            if (term.trim()) {
-              return <SearchList term={term} />;
-            } else {
-              return <ListTypesList />;
-            }
-          }}
-        </SearchConsumer>
-      </>
+          if (term.trim()) {
+            return <SearchList term={term} />;
+          } else {
+            return <ListTypesList />;
+          }
+        }}
+      </SearchConsumer>
     );
   }
 }
