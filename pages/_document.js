9433f98938614162b30d2a84cc72d143392df195
Manuel Ruck <github@manuelruck.de>
2018-10-01 16:35:18 +0200

    improve favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,11 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import getConfig from 'next/config';
+
+// CONFIGS
+const {
+  publicRuntimeConfig: { DOMAIN_DESKTOP },
+} = getConfig();
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -14,7 +20,95 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="57x57"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-57x57.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="114x114"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-114x114.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-72x72.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-144x144.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="60x60"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-60x60.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-120x120.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="76x76"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-76x76.png`}
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href={`${DOMAIN_DESKTOP}/apple-touch-icon-152x152.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${DOMAIN_DESKTOP}/favicon-196x196.png`}
+            sizes="196x196"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${DOMAIN_DESKTOP}/favicon-96x96.png`}
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${DOMAIN_DESKTOP}/favicon-32x32.png`}
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${DOMAIN_DESKTOP}/favicon-16x16.png`}
+            sizes="16x16"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${DOMAIN_DESKTOP}/favicon-128.png`}
+            sizes="128x128"
+          />
+          <meta name="application-name" content="DEMOCRACY" />
+          <meta name="msapplication-TileColor" content="#4494d3" />
+          <meta name="msapplication-TileImage" content={`${DOMAIN_DESKTOP}/mstile-144x144.png`} />
+          <meta
+            name="msapplication-square70x70logo"
+            content={`${DOMAIN_DESKTOP}/mstile-70x70.png`}
+          />
+          <meta
+            name="msapplication-square150x150logo"
+            content={`${DOMAIN_DESKTOP}/mstile-150x150.png`}
+          />
+          <meta
+            name="msapplication-wide310x150logo"
+            content={`${DOMAIN_DESKTOP}/mstile-310x150.png`}
+          />
+          <meta
+            name="msapplication-square310x310logo"
+            content={`${DOMAIN_DESKTOP}/mstile-310x310.png`}
+          />
           {styleTags}
         </Head>
         <body>
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 require('dotenv/config');
 const express = require('express');
-var favicon = require('serve-favicon');
 const next = require('next');
+const { join } = require('path');
+const { parse } = require('url');
 
 const Router = require('./routes').Router;
 
@@ -9,7 +10,6 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-var path = require('path');
 
 app
   .prepare()
@@ -21,10 +21,38 @@ app
         app.render(req, res, `/${page}`, Object.assign({}, defaultParams, req.query, req.params)),
       ),
     );
+    // path.join(__dirname, '../static', 'favicon.ico')
 
-    server.use(favicon(path.join(__dirname, '../static', 'favicon.ico')));
+    server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      const rootStaticFiles = [
+        '/apple-touch-icon-57x57.png',
+        '/apple-touch-icon-60x60.png',
+        '/apple-touch-icon-72x72.png',
+        '/apple-touch-icon-76x76.png',
+        '/apple-touch-icon-114x114.png',
+        '/apple-touch-icon-120x120.png',
+        '/apple-touch-icon-144x144.png',
+        '/apple-touch-icon-152x152.png',
+        '/favicon-16x16.png',
+        '/favicon-32x32.png',
+        '/favicon-96x96.png',
+        '/favicon-128.png',
+        '/favicon-196x196.png',
+        '/favicon.ico',
+        '/mstile-70x70.png',
+        '/mstile-144x144.png',
+        '/mstile-150x150.png',
+        '/mstile-310x150.png',
+        '/mstile-310x310.png',
+      ];
 
-    server.get('*', (req, res) => handle(req, res));
+      if (rootStaticFiles.indexOf(parsedUrl.pathname) > -1) {
+        const path = join(__dirname, '../static', parsedUrl.pathname);
+        return app.serveStatic(req, res, path);
+      }
+      return handle(req, res);
+    });
     server.listen(port);
   })
   .catch(ex => {
