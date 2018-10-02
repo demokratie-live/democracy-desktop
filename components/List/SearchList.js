232eb3c025a87c4a96a61e6b190965840ee447c9
Manuel Ruck <github@manuelruck.de>
2018-10-02 10:30:17 +0200

    fix suche without results

--- components/List/SearchList.js
+++ components/List/SearchList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import styled from 'styled-components';
 
+import { Spin as AntSpin } from 'antd';
 import TeaserList from './TeaserList';
 
 // Context
@@ -17,6 +18,21 @@ const Section = styled.section`
   padding-right: ${({ theme }) => theme.space(4)}px;
   padding-top: ${({ theme }) => theme.space(2)}px;
   padding-bottom: ${({ theme }) => theme.space(4)}px;
+`;
+
+const SpinWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+`;
+const Spin = styled(AntSpin)``;
+
+const NoResultsWrapper = styled.div`
+  min-height: 92px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
 `;
 
 class SearchList extends Component {
@@ -35,10 +51,20 @@ class SearchList extends Component {
                   }}
                 >
                   {({ loading, error, data }) => {
-                    if (loading) return <p>Loading...</p>;
+                    if (loading)
+                      return (
+                        <SpinWrapper>
+                          <Spin size="large" tip="Lädt…" />
+                        </SpinWrapper>
+                      );
 
                     if (error) {
                       return <p>Error :(</p>;
+                    }
+                    if (data.searchProceduresAutocomplete.procedures.length === 0) {
+                      return (
+                        <NoResultsWrapper>Ihre Suche ergab leider keine Treffer</NoResultsWrapper>
+                      );
                     }
                     return (
                       <TeaserList
